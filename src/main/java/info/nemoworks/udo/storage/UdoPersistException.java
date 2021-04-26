6c80866949321df230dfae2328578280674ead0e
Chun <caochun@gmail.com>
2021-04-26 22:52:48 +0800

    exception fix

--- src/main/java/info/nemoworks/udo/storage/UdoPersistException.java
+++ src/main/java/info/nemoworks/udo/storage/UdoPersistException.java
@@ -4,8 +4,6 @@ import info.nemoworks.udo.model.UdoException;
 
 public class UdoPersistException extends UdoException {
 
-    private static final long serialVersionUID = 1L;
-
     public UdoPersistException(String message) {
         super(message);
     }
